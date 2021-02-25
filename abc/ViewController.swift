681f204f8a8b4ce83e9a888728edb4dcc0e4a569
Ashish Mishra <ashish.mishra@wynk.in>
2021-02-25 23:35:31 +0530

    intial commit

---
+++ abc/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  abc
+//
+//  Created by B0212150 on 25/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func helloButtonClicked() {
+        print("HEllo Tapped!!!")
+    }
+
+
+}
+
